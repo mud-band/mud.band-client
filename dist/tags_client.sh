4fcf6f4ca5f512eb9d62c631aaa7a769debde13f
Weongyo Jeong <weongyo@mud.band>
2025-01-26 17:37:37 -0800

    Code cleanup.

--- dist/tags_client.sh
+++ dist/tags_client.sh
@@ -3,9 +3,6 @@
 NOW=`date +%Y%m%d%H%M%S`
 VER=0.0.6
 
-echo "Tags the Mudband v$VER-$NOW."
-git pull
+echo "Tags the Mud.band client v$VER-$NOW."
 git tag -a client-v$VER-$NOW -m "Tags v$VER-$NOW"
-git pull
-git push
 git push origin client-v$VER-$NOW
