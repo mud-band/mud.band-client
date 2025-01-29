bbb5d3caa9751bb194a70f4d3a274ecb76edc242
Weongyo Jeong <weongyo@mud.band>
2025-01-28 20:36:05 -0800

    Updates the package script for windows.

--- dist/windows/dist.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-VERSION=0.0.7
-
-scp ../releases/${VERSION}/windows/x86/mudband-${VERSION}-windows-x86.zip \
-    ../releases/${VERSION}/windows/x86_64/mudband-${VERSION}-windows-x86_64.zip
-    45.33.41.192:/opt/www.mud.band/htdocs/releases/
--- dist/windows/package_x86_64.bat
+++ dist/windows/package_x86_64.bat
@@ -3,14 +3,13 @@ set VERSION=0.0.7
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 cd ..
 mkdir releases\%VERSION%\windows\x86_64
-cd ..\bin\mudband
+cd ..\bin\mudband\win32
 nmake -f NMakefile.x64 clean
 nmake -f NMakefile.x64 BUILD=RELEASE
 signtool sign /fd sha256 /n Mudfish /t http://timestamp.comodoca.com/authenticode mudband.exe
-copy mudband.exe ..\..\dist\releases\%VERSION%\windows\x86_64\
-cd ..\..\
+copy mudband.exe ..\..\..\dist\releases\%VERSION%\windows\x86_64\
+copy mudband.pdb ..\..\..\dist\releases\%VERSION%\windows\x86_64\
+cd ..\..\..\
 copy contrib\prebuilt\wintun\bin\amd64\wintun.dll dist\releases\%VERSION%\windows\x86_64\
 cd dist\releases\%VERSION%\windows\x86_64\
-7z a mudband-%VERSION%-windows-x86_64.zip mudband.exe wintun.dll
-cd ..\..\..\..\..\bin\mudband
-copy mudband.pdb ..\..\dist\releases\%VERSION%\windows\x86_64
+7z a mudband-%VERSION%-windows-x86_64.zip mudband.exe mudband.pdb wintun.dll
