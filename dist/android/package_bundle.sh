f4482787169866e35daebbef4156633222629bf6
Mud.band <contact@mud.band>
2025-02-01 22:47:07 -0800

    Builds the app bundle. (#35)

---
+++ dist/android/package_bundle.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+TOPDIR=../../bin/mudband/android/Mudband
+VERSION=0.0.9
+
+(cd ${TOPDIR} && ./gradlew clean && ./gradlew bundle)
+
+mkdir -p ../releases/${VERSION}/android/
+
+cp ${TOPDIR}/app/build/outputs/bundle/debug/app-debug.aab \
+    ../releases/${VERSION}/android/mudband-${VERSION}-debug.aab
+cp ${TOPDIR}/app/build/outputs/bundle/release/app-release.aab \
+    ../releases/${VERSION}/android/mudband-${VERSION}-release.aab
