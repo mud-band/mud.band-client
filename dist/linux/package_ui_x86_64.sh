e2bb8d5d06f446e915252c4758514297277c31d1
Weongyo Jeong <weongyo@mud.band>
2025-02-10 19:23:17 -0800

    linux: adds dist/linux/package_ui_x86_64.sh

---
+++ dist/linux/package_ui_x86_64.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+TOPDIR=../../bin/mudband_ui/linux
+BUNDLEDIR=${TOPDIR}/src-tauri/target/release/bundle
+VERSION=0.0.9
+
+(cd ${TOPDIR} && make build)
+
+mkdir -p ../releases/${VERSION}/linux/
+
+cp ${BUNDLEDIR}/deb/mudband-ui_${VERSION}_amd64.deb \
+    ../releases/${VERSION}/linux/
+cp ${BUNDLEDIR}/rpm/mudband-ui-${VERSION}-1.x86_64.rpm \
+    ../releases/${VERSION}/linux/
+cp ${BUNDLEDIR}/appimage/mudband-ui_${VERSION}_amd64.AppImage \
+    ../releases/${VERSION}/linux/
