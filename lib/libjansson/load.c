5df6f1b6c5007d3f1fd40419c4544af544ed8a91
Weongyo Jeong <weongyo@mud.band>
2025-03-06 17:28:25 -0800

    Uses sizeof().

--- lib/libjansson/load.c
+++ lib/libjansson/load.c
@@ -92,8 +92,8 @@ static void error_set(json_error_t *error, const lex_t *lex,
         return;
 
     va_start(ap, msg);
-    vsnprintf(msg_text, JSON_ERROR_TEXT_LENGTH, msg, ap);
-    msg_text[JSON_ERROR_TEXT_LENGTH - 1] = '\0';
+    vsnprintf(msg_text, sizeof(msg_text), msg, ap);
+    msg_text[sizeof(msg_text) - 1] = '\0';
     va_end(ap);
 
     if(lex)
@@ -107,9 +107,9 @@ static void error_set(json_error_t *error, const lex_t *lex,
         if(saved_text && saved_text[0])
         {
             if(lex->saved_text.length <= 20) {
-                snprintf(msg_with_context, JSON_ERROR_TEXT_LENGTH,
+                snprintf(msg_with_context, sizeof(msg_with_context),
                          "%s near '%s'", msg_text, saved_text);
-                msg_with_context[JSON_ERROR_TEXT_LENGTH - 1] = '\0';
+                msg_with_context[sizeof(msg_with_context) - 1] = '\0';
                 result = msg_with_context;
             }
         }
@@ -120,9 +120,9 @@ static void error_set(json_error_t *error, const lex_t *lex,
                 result = msg_text;
             }
             else {
-                snprintf(msg_with_context, JSON_ERROR_TEXT_LENGTH,
+                snprintf(msg_with_context, sizeof(msg_with_context),
                          "%s near end of file", msg_text);
-                msg_with_context[JSON_ERROR_TEXT_LENGTH - 1] = '\0';
+                msg_with_context[sizeof(msg_with_context) - 1] = '\0';
                 result = msg_with_context;
             }
         }
